0c79d43036dcc76a9e3ae21e1cdf7ecd9f98f29c
IJou <ijou86218@gmail.com>
2018-01-09 22:03:36 +0800

    init

---
+++ City.java
@@ -0,0 +1,48 @@
+// 初始化基本位置
+public class City {
+    int x; // x座標
+    int y; // y座標
+    String cityName;
+
+    // Constructs a randomly placed city 如果沒有預設座標，取亂數
+    public City(){
+        this.x = (int)(Math.random()*200);
+        this.y = (int)(Math.random()*200);
+    }
+
+    // Constructs a city at chosen x, y location
+    public City(String cityName, int x, int y){
+        this.cityName = cityName;
+        this.x = x;
+        this.y = y;
+    }
+
+    // Gets city's name
+    public String getName(){
+        return this.cityName;
+    }
+
+    // Gets city's x coordinate
+    public int getX(){
+        return this.x;
+    }
+
+    // Gets city's y coordinate
+    public int getY(){
+        return this.y;
+    }
+
+    // Gets the distance to given city 取得距離
+    public double distanceTo(City city){
+        int xDistance = Math.abs(getX() - city.getX());
+        int yDistance = Math.abs(getY() - city.getY());
+        double distance = Math.sqrt( (xDistance*xDistance) + (yDistance*yDistance) );
+
+        return distance;
+    }
+
+    @Override
+    public String toString(){
+        return getName()+":"+getX()+", "+getY();
+    }
+}
---
+++ TSP_GA.java
@@ -0,0 +1,69 @@
+import java.util.Scanner;
+
+public class TSP_GA {
+    public static void main(String[] args) {
+        // Create and add our cities
+        String str;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please Enter Ur Identity: ");
+        System.out.println("(Old/Pregnant/Children/Normal)");
+        str = input.next();
+        
+        City city = new City("C1", 60, 200);
+        TourManager.addCity(city);
+        City city2 = new City("C2", 180, 200);
+        TourManager.addCity(city2);
+        City city3 = new City("C3", 80, 180);
+        TourManager.addCity(city3);
+        City city4 = new City("C4", 140, 180);
+        TourManager.addCity(city4);
+        City city5 = new City("C5", 20, 160);
+        TourManager.addCity(city5);
+        City city6 = new City("C6", 100, 160);
+        TourManager.addCity(city6);
+        City city7 = new City("C7", 200, 160);
+        TourManager.addCity(city7);
+        City city8 = new City("C8", 140, 140);
+        TourManager.addCity(city8);
+        City city9 = new City("C9", 40, 120);
+        TourManager.addCity(city9);
+        City city10 = new City("C10", 100, 120);
+        TourManager.addCity(city10);
+        City city11 = new City("C11", 180, 100);
+        TourManager.addCity(city11);
+        City city12 = new City("C12", 60, 80);
+        TourManager.addCity(city12);
+        City city13 = new City("C13", 120, 80);
+        TourManager.addCity(city13);
+        City city14 = new City("C14", 180, 60);
+        TourManager.addCity(city14);
+        City city15 = new City("C15", 20, 40);
+        TourManager.addCity(city15);
+        City city16 = new City("C16", 100, 40);
+        TourManager.addCity(city16);
+        City city17 = new City("C17", 200, 40);
+        TourManager.addCity(city17);
+        City city18 = new City("C18", 20, 20);
+        TourManager.addCity(city18);
+        City city19 = new City("C19", 60, 20);
+        TourManager.addCity(city19);
+        City city20 = new City("C20", 160, 20);
+        TourManager.addCity(city20);
+
+        // Initialize population
+        Population pop = new Population(50, true);
+        System.out.println("Initial distance: " + pop.getFittest().getDistance());
+
+        // Evolve population for 100 generations
+        pop = GA.evolvePopulation(pop);
+        for (int i = 0; i < 100; i++) {
+            pop = GA.evolvePopulation(pop);
+        }
+
+        // Print final results
+        System.out.println("Finished");
+        System.out.println("Final distance: " + pop.getFittest().getDistance());
+        System.out.println("Solution:");
+        System.out.println(pop.getFittest());
+    }
+}
