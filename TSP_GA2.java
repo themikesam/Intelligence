50f862cfe82abd862033be0703d03ef7fcdd33cf
mikesam9565 <samson9565@gmail.com>
2018-01-11 00:32:33 +0800

    更新忽略case

--- TSP_GA2.java
+++ TSP_GA2.java
@@ -91,9 +91,9 @@ static int xPoly[] = new int [50];
         System.out.println("(Old/Pregnant/Children/Normal)");
         System.out.println("Please Enter Ur Identity: ");
         str = input.next();
-        int[] Old = {1,2,3}; 
-        int[] Pregnant = {4, 5, 6}; 
-        int[] Children = {7,8,9}; 
+        int[] Old = {2,3,4,5,7,8,9,13,14,15,17,23,25,28,30,34; 
+        int[] Pregnant = {2,3,4,5,7,8,9,13,14,15,17,20,21,23,25,28,30,31,33,34}; 
+        int[] Children = {5,7,9,10,12,13,14,15,23,30}; 
         if (str.equals("Old")) {
             for(int k=0;k<Old.length;k++){
                 TourManager.removeCity(city[Old[k]-1]);
