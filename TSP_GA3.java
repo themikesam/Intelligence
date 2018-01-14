de24c946e3977a1ac2a8ed61ba6dda3f209f11e0
mikesam9565 <samson9565@gmail.com>
2018-01-14 17:17:48 +0800

    新增畫原始路徑的功能

--- TSP_GA3.java
+++ TSP_GA3.java
@@ -8,13 +8,16 @@ import javax.swing.SwingUtilities;
 
 public class TSP_GA3 extends JFrame {
     public JFrame mainMap;
-    public Polygon poly;
+    public Polygon poly0;
+    public Polygon poly1;
     public Polygon poly2;
     public TSP_GA3() {
         initComponents();
     }
-    static int xPoly[] = new int [50];
-    static int yPoly[] = new int [50];
+    static int x0Poly[] = new int [50];
+    static int y0Poly[] = new int [50];
+    static int x1Poly[] = new int [50];
+    static int y1Poly[] = new int [50];
     static int x2Poly[] = new int [50];
     static int y2Poly[] = new int [50];
     public Image image;
@@ -22,20 +25,23 @@ public class TSP_GA3 extends JFrame {
         mainMap = new JFrame();
         mainMap.setResizable(false);
         mainMap.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        poly = new Polygon(xPoly, yPoly, TourManager.getSize());
+        poly0 = new Polygon(x0Poly, y0Poly, TourManager.getSize());
+        poly1 = new Polygon(x1Poly, y1Poly, TourManager.getSize());
         poly2 = new Polygon(x2Poly, y2Poly, TourManager.getSize());
         JPanel p = new JPanel() {
             @Override
             protected void paintComponent(Graphics g) {
                 try {
-                    image = ImageIO.read(new File("lulu.jpg"));
+                    image = ImageIO.read(new File("lulu1.jpg"));
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
                 super.paintComponent(g);
                 g.drawImage(image, 0, 0, 800, 600, null);
+                // g.setColor(Color.GREEN);
+                // g.drawPolygon(poly0);
                 g.setColor(Color.BLUE);
-                g.drawPolygon(poly);
+                g.drawPolygon(poly1);
                 g.setColor(Color.RED);
                 g.drawPolygon(poly2);
             }
@@ -59,7 +65,7 @@ public class TSP_GA3 extends JFrame {
         String str;
         Scanner input = new Scanner(System.in);
         // Open this file.
-        BufferedReader reader = new BufferedReader(new FileReader(("test1.txt")));
+        BufferedReader reader = new BufferedReader(new FileReader(("orginal_java.txt")));
         // Read lines from file.
         City[] city = new City[34];
         int i = 0;
@@ -98,7 +104,10 @@ public class TSP_GA3 extends JFrame {
         }
 
         System.out.println(TourManager.getSize());
-
+        for(int k = 0; k < TourManager.getSize(); k++){
+            x0Poly[k] = (int)(city[k].getX() * 8.25 - 20);
+            y0Poly[k] = (int)(city[k].getY() * 8.2 + 140);
+        }
         // Initialize population
         Population pop = new Population(50, true);
         System.out.println("Initial\nDistance: " + pop.getFittest().getDistance());
@@ -109,11 +118,11 @@ public class TSP_GA3 extends JFrame {
         writer_before.close();
         BufferedReader reader_before = new BufferedReader(new FileReader(("result_before.txt")));
         // Read lines from file.
-        int m = 0;
+        int c_before = 0;
         String[] name_before = new String [100];
         String[] x_before = new String [100];
         String[] y_before = new String [100];
-        while (m!=TourManager.getSize()) {
+        while (c_before!=TourManager.getSize()) {
             String line_result_before = reader_before.readLine();
             if (line_result_before == null) {
                 break;
@@ -122,15 +131,15 @@ public class TSP_GA3 extends JFrame {
             }
             String[] value = line_result_before.split(",");
             // System.out.println(l+":"+value.length); //debug use
-            name_before[m] = value[0];
-            x_before[m] = value[1];
-            y_before[m] = value[2];
-            m++;
+            name_before[c_before] = value[0];
+            x_before[c_before] = value[1];
+            y_before[c_before] = value[2];
+            c_before++;
         }
         reader_before.close();
         for(int k = 0; k < TourManager.getSize(); k++){
-            xPoly[k] = (int)(Integer.parseInt(x_before[k] )* 8.25 - 20);
-            yPoly[k] = (int)(Integer.parseInt(y_before[k].substring(0, y_before[k].length() - 1))* 8.2 + 140);
+            x1Poly[k] = (int)(Integer.parseInt(x_before[k] )* 8.25 - 20);
+            y1Poly[k] = (int)(Integer.parseInt(y_before[k].substring(0, y_before[k].length() - 1))* 8.2 + 140);
         }
         // Evolve population for 100 generations
         pop = GA.evolvePopulation(pop);
@@ -139,46 +148,37 @@ public class TSP_GA3 extends JFrame {
         }
 
         // Print final results
-        System.out.println("\n\nEvolution Finished\n\n");
-        System.out.println("Final");
-        System.out.println("Distance: " + pop.getFittest().getDistance());
+        System.out.println("\n\nEvolution Finished\n\nFinal\nDistance: " + pop.getFittest().getDistance());
         System.out.println("Path:");
         System.out.println(pop.getFittest());
-        PrintWriter writer = new PrintWriter("result.txt", "UTF-8");
-        writer.println(pop.getFittest());
-        writer.close();
-        BufferedReader reader1 = new BufferedReader(new FileReader(("result.txt")));
+        PrintWriter writer_after = new PrintWriter("result_after.txt", "UTF-8");
+        writer_after.println(pop.getFittest());
+        writer_after.close();
+        BufferedReader reader_after = new BufferedReader(new FileReader(("result_after.txt")));
         // Read lines from file.
-        int l = 0;
+        int c_after = 0;
         String[] name = new String [100];
         String[] x = new String [100];
         String[] y = new String [100];
-        while (l!=TourManager.getSize()) {
-            String line_result = reader1.readLine();
-            if (line_result == null) {
+        while (c_after!=TourManager.getSize()) {
+            String line_result_after = reader_after.readLine();
+            if (line_result_after == null) {
                 break;
-            } else if(line_result.contains("\n") || line_result.contains("\r\n")) {
+            } else if(line_result_after.contains("\n") || line_result_after.contains("\r\n")) {
                 break;
             }
-            String[] value = line_result.split(",");
+            String[] value = line_result_after.split(",");
             // System.out.println(l+":"+value.length);
-            name[l] = value[0];
-            x[l] = value[1];
-            y[l] = value[2];
-            l++;
+            name[c_after] = value[0];
+            x[c_after] = value[1];
+            y[c_after] = value[2];
+            c_after++;
         }
-        reader1.close();
+        reader_after.close();
         for(int k = 0; k < TourManager.getSize(); k++){
             x2Poly[k] = (int)(Integer.parseInt(x[k] )* 8.25 - 20);
             y2Poly[k] = (int)(Integer.parseInt(y[k].substring(0, y[k].length() - 1))* 8.2 + 140);
         }
-        for(int k = 0;k<TourManager.getSize();k++){
-            // System.out.print(name[k].substring(1));
-            // System.out.print(x[k]);
-            // System.out.print(y[k].substring(0, y[k].length() - 1));
-            // System.out.println();
-        }
-
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
